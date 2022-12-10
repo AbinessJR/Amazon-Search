08f3a021a4ff85fcceddf8019c6e2778166b94a7
ABINESS-JR <iamabiness@gmail.com>
2022-12-10 11:55:26 +0530

    Commit one

---
+++ AmazonN1/src/main/java/com/qa/testscripts/AmazonSearch.java
@@ -0,0 +1,61 @@
+package com.qa.testscripts;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.qa.pages.Amazonpages;
+import com.qa.utility.ExcelUtility;
+
+public class AmazonSearch extends Testbase{
+	
+	@DataProvider(name="getdata")
+	public String[][] getData() throws IOException {
+		
+		String xFile="C:\\Users\\user\\Downloads\\AmazonN1\\src\\main\\java\\com\\qa\\testdata\\DataAMZN.xlsx";
+		String xSheet="Sheet1";
+		
+		int rowCount = ExcelUtility.getRowCount(xFile, xSheet);
+		int cellCount = ExcelUtility.getCellCount(xFile, xSheet, rowCount);
+		System.out.println("rows = "+rowCount);
+		System.out.println("cells = "+cellCount);
+		
+		String[][] data = new String[rowCount][cellCount];
+		
+		for(int i=1; i<=rowCount; i++) {
+			for(int j=0;j<cellCount;j++) {
+				data[i-1][j] = ExcelUtility.getCellData(xFile, xSheet, i, j);
+			}
+		}
+		return data;
+}
+	@Test(dataProvider = "getdata")
+	public void getTest(String catagory, String items ) throws IOException, InterruptedException {
+	PageFactory.initElements(driver, Amazonpages.class);
+	Thread.sleep(5000);
+	
+	Amazonpages.selectCatagory.click();
+	Select slct=new Select(Amazonpages.selectCatagory);
+	Thread.sleep(3000);
+	 slct.selectByVisibleText(catagory);
+	Amazonpages.amznSearchBox.sendKeys(items+Keys.ENTER);
+	Thread.sleep(3000);
+	 Actions at=new Actions(driver);
+		
+	at.sendKeys(Keys.PAGE_DOWN).build().perform();
+	 Thread.sleep(3000);
+	 at.sendKeys(Keys.PAGE_UP).build().perform();
+	
+	driver.navigate().to("https://www.amazon.in/");
+}
+}
+
